2594abad581f783c8bf20a95db07e88eee6ccb5f
Sanya Sogani <sanya@thehousemonk.com>
2022-05-26 13:06:04 +0530

    added some filters and pagination

---
+++ js/app/controllers/StoryController.js
@@ -0,0 +1,26 @@
+(function() {
+'use strict';
+
+  angular
+    .module('app')
+    .controller('StoryController', StoryController);
+
+  StoryController.$inject = ['TopStoriesService', '$stateParams', 'story'];
+  function StoryController(TopStoriesService, $stateParams, story) {
+    var vm = this;
+    
+    
+
+    activate();
+
+    ////////////////
+
+    function activate() {
+      vm.id = $stateParams.id;
+      vm.story = story.data;
+      vm.comments = story.data.kids;
+      
+
+    }
+  }
+})();
